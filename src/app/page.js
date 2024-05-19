ccc068509c650ac3d66d1fce6660d8b168219f85
bekzoddr <149781755+bekzoddr@users.noreply.github.com>
2024-05-19 18:01:28 +0500

    comment

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,20 @@
 import Image from "next/image";
 import admin from "@/assets/images/admin.gif";
+import Modal from "@/components/modal/Modal";
 export default function Home() {
   return (
-    <div className="home">
-      <Image src={admin} alt="admin" />
-      <marquee behavior="" direction="left">
-        <div className="text">
-          <h1 className="one">Welcome to admin panel</h1>
-          <h1 className="two">Welcome to admin panel</h1>
-          <h1 className="three">Welcome to admin panel</h1>
-        </div>
-      </marquee>
-    </div>
+    <>
+      <div className="home">
+        <Image src={admin} alt="admin" />
+        <marquee behavior="" direction="left">
+          <div className="text">
+            <h1 className="one">Welcome to admin panel</h1>
+            <h1 className="two">Welcome to admin panel</h1>
+            <h1 className="three">Welcome to admin panel</h1>
+          </div>
+        </marquee>
+        <Modal />
+      </div>
+    </>
   );
 }
